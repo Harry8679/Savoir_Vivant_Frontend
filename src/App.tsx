13603b79ad556f350e195cc66b665740cedeed8f
Harry7986 <emarh.harry.code@gmail.com>
2026-03-24 17:26:03 +0100

    App 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -13,6 +10,4 @@ function App() {
   )
 }
 
-export default App
-
-export default App
+export default App;
