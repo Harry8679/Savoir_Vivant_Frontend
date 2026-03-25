d35a81eb1edc5f3cdfff49e6ee4189f1fbcf8ad5
Harry7986 <emarh.harry.code@gmail.com>
2026-03-25 12:59:29 +0100

    Catalogue

--- src/pages/Catalogue/CataloguePage.tsx
+++ src/pages/Catalogue/CataloguePage.tsx
@@ -1,7 +1,3 @@
-const CataloguePage = () => {
-  return (
-    <div>CataloguePage</div>
-  )
+export default function CataloguePage() {
+  return <div style={{ padding: '8rem 3rem', color: 'var(--color-text)' }}>Catalogue</div>
 }
-
-export default CataloguePage
