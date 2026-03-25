41498463d19aa6b53a3df2d8f876fa26c2ea02e2
Harry7986 <emarh.harry.code@gmail.com>
2026-03-25 14:07:42 +0100

    routes 2

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,14 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '@pages/Home/HomePage'
 import LoginPage from '@pages/Auth/LoginPage'
 import RegisterPage from '@pages/Auth/RegisterPage'
+import ForgotPasswordPage from '@pages/Auth/ForgotPasswordPage'
 import ProfilePage from '@pages/Profile/ProfilePage'
 import CataloguePage from '@pages/Catalogue/CataloguePage'
 import LibraryPage from '@pages/Library/LibraryPage'
+import NotFoundPage from '@pages/NotFound/NotFoundPage'
+import CguPage from '@pages/Legal/CguPage'
+import ConfidentialitePage from '@pages/Legal/ConfidentialitePage'
 import RootLayout from '@components/layout/RootLayout'
-import NotFoundPage from '@/pages/NotFound/NotFoundPage'
 
 export const router = createBrowserRouter([
   {
@@ -17,10 +20,15 @@ export const router = createBrowserRouter([
       { path: 'catalogue', element: <CataloguePage /> },
       { path: 'profile', element: <ProfilePage /> },
       { path: 'library', element: <LibraryPage /> },
+      { path: 'cgu', element: <CguPage /> },
+      { path: 'confidentialite', element: <ConfidentialitePage /> },
     ],
   },
+  { path: '/login', element: <LoginPage /> },
   { path: '/connexion', element: <LoginPage /> },
   { path: '/register', element: <RegisterPage /> },
   { path: '/inscription', element: <RegisterPage /> },
+  { path: '/forgot-password', element: <ForgotPasswordPage /> },
+  { path: '/mot-de-passe-oublie', element: <ForgotPasswordPage /> },
   { path: '*', element: <NotFoundPage /> },
 ])
