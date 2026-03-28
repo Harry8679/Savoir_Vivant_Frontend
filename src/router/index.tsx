14ab26d46e4511b5e260856d826e14c21a2df86d
Harry7986 <emarh.harry.code@gmail.com>
2026-03-28 23:50:31 +0100

    Routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,37 +1,45 @@
 import { createBrowserRouter } from 'react-router-dom'
-import HomePage from '@pages/Home/HomePage'
-import LoginPage from '@pages/Auth/LoginPage'
-import RegisterPage from '@pages/Auth/RegisterPage'
+import HomePage           from '@pages/Home/HomePage'
+import LoginPage          from '@pages/Auth/LoginPage'
+import RegisterPage       from '@pages/Auth/RegisterPage'
 import ForgotPasswordPage from '@pages/Auth/ForgotPasswordPage'
-import ProfilePage from '@pages/Profile/ProfilePage'
-import CataloguePage from '@pages/Catalogue/CataloguePage'
-import LibraryPage from '@pages/Library/LibraryPage'
-import NotFoundPage from '@pages/NotFound/NotFoundPage'
-import CguPage from '@pages/Legal/CguPage'
+import ProfilePage        from '@pages/Profile/ProfilePage'
+import CataloguePage      from '@pages/Catalogue/CataloguePage'
+import LibraryPage        from '@pages/Library/LibraryPage'
+import OrdersPage         from '@pages/Orders/OrdersPage'
+import SubscriptionPage   from '@pages/Subscription/SubscriptionPage'
+import SettingsPage       from '@pages/Settings/SettingsPage'
+import NotFoundPage       from '@pages/NotFound/NotFoundPage'
+import CguPage            from '@pages/Legal/CguPage'
 import ConfidentialitePage from '@pages/Legal/ConfidentialitePage'
-import RootLayout from '@components/layout/RootLayout'
-import PrivateRoute from './PrivateRoute'
+import RootLayout         from '@components/layout/RootLayout'
+import PrivateRoute       from './PrivateRoute'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { index: true,      element: <HomePage /> },
-      { path: 'catalogue', element: <CataloguePage /> },
-      { path: 'cgu',       element: <CguPage /> },
+      { index: true,             element: <HomePage /> },
+      { path: 'catalogue',       element: <CataloguePage /> },
+      { path: 'cgu',             element: <CguPage /> },
       { path: 'confidentialite', element: <ConfidentialitePage /> },
       {
         element: <PrivateRoute />,
         children: [
-          { path: 'profile', element: <ProfilePage /> },
-          { path: 'library', element: <LibraryPage /> },
+          { path: 'profile',      element: <ProfilePage /> },
+          { path: 'library',      element: <LibraryPage /> },
+          { path: 'orders',       element: <OrdersPage /> },
+          { path: 'subscription', element: <SubscriptionPage /> },
+          { path: 'settings',     element: <SettingsPage /> },
         ],
       },
     ],
   },
   { path: '/connexion',           element: <LoginPage /> },
+  { path: '/login',               element: <LoginPage /> },
   { path: '/inscription',         element: <RegisterPage /> },
+  { path: '/register',            element: <RegisterPage /> },
   { path: '/forgot-password',     element: <ForgotPasswordPage /> },
   { path: '/mot-de-passe-oublie', element: <ForgotPasswordPage /> },
   { path: '*',                    element: <NotFoundPage /> },
