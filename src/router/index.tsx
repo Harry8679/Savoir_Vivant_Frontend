da645df7c1cf80f3ee0e8c41ed41657fb5f5d3cc
Harry7986 <emarh.harry.code@gmail.com>
2026-03-29 16:16:14 +0200

    Routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,29 +1,31 @@
 import { createBrowserRouter } from 'react-router-dom'
-import HomePage           from '@pages/Home/HomePage'
-import LoginPage          from '@pages/Auth/LoginPage'
-import RegisterPage       from '@pages/Auth/RegisterPage'
-import ForgotPasswordPage from '@pages/Auth/ForgotPasswordPage'
-import ProfilePage        from '@pages/Profile/ProfilePage'
-import CataloguePage      from '@pages/Catalogue/CataloguePage'
-import LibraryPage        from '@pages/Library/LibraryPage'
-import OrdersPage         from '@pages/Orders/OrdersPage'
-import SubscriptionPage   from '@pages/Subscription/SubscriptionPage'
-import SettingsPage       from '@pages/Settings/SettingsPage'
-import NotFoundPage       from '@pages/NotFound/NotFoundPage'
-import CguPage            from '@pages/Legal/CguPage'
+import HomePage            from '@pages/Home/HomePage'
+import LoginPage           from '@pages/Auth/LoginPage'
+import RegisterPage        from '@pages/Auth/RegisterPage'
+import ForgotPasswordPage  from '@pages/Auth/ForgotPasswordPage'
+import ProfilePage         from '@pages/Profile/ProfilePage'
+import CataloguePage       from '@pages/Catalogue/CataloguePage'
+import BookDetailPage      from '@pages/Catalogue/BookDetailPage'
+import LibraryPage         from '@pages/Library/LibraryPage'
+import OrdersPage          from '@pages/Orders/OrdersPage'
+import SubscriptionPage    from '@pages/Subscription/SubscriptionPage'
+import SettingsPage        from '@pages/Settings/SettingsPage'
+import NotFoundPage        from '@pages/NotFound/NotFoundPage'
+import CguPage             from '@pages/Legal/CguPage'
 import ConfidentialitePage from '@pages/Legal/ConfidentialitePage'
-import RootLayout         from '@components/layout/RootLayout'
-import PrivateRoute       from './PrivateRoute'
+import RootLayout          from '@components/layout/RootLayout'
+import PrivateRoute        from './PrivateRoute'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { index: true,             element: <HomePage /> },
-      { path: 'catalogue',       element: <CataloguePage /> },
-      { path: 'cgu',             element: <CguPage /> },
-      { path: 'confidentialite', element: <ConfidentialitePage /> },
+      { index: true,                element: <HomePage /> },
+      { path: 'catalogue',          element: <CataloguePage /> },
+      { path: 'catalogue/:slug',    element: <BookDetailPage /> },
+      { path: 'cgu',                element: <CguPage /> },
+      { path: 'confidentialite',    element: <ConfidentialitePage /> },
       {
         element: <PrivateRoute />,
         children: [
@@ -37,7 +39,10 @@ export const router = createBrowserRouter([
     ],
   },
   { path: '/connexion',           element: <LoginPage /> },
+  { path: '/login',               element: <LoginPage /> },
   { path: '/inscription',         element: <RegisterPage /> },
+  { path: '/register',            element: <RegisterPage /> },
+  { path: '/forgot-password',     element: <ForgotPasswordPage /> },
   { path: '/mot-de-passe-oublie', element: <ForgotPasswordPage /> },
   { path: '*',                    element: <NotFoundPage /> },
 ])
