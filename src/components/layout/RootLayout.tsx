0c1bf4ef93042bd114961143bc00155572f26d23
Harry7986 <emarh.harry.code@gmail.com>
2026-03-31 12:36:09 +0200

    Root Layout

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useInitAuth } from '@hooks/useInitAuth'
 
 export default function RootLayout() {
+  useInitAuth()
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
